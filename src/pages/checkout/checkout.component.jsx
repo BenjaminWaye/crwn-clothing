c6a73542adbf96c7daddc7cb7ae37e10fcfab588
Benjamin Waye <benjamin@waye.co>
2020-04-15 14:21:18 +0200

    added addItem and removItem functionality to checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -23,6 +23,9 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
         <span>Quantity</span>
       </div>
       <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
